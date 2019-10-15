daea3d9cec9b0ef17be923a18389ec7ae8dd904c
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2019-10-15 16:56:55 +0200

    [FIX] inverse suggested calculation

--- housing_cooperative_base/models/housing.py
+++ housing_cooperative_base/models/housing.py
@@ -83,11 +83,11 @@ class Housing(models.Model):
         for housing in self:
             if housing.building_id.regime == 'square_meters':
                 housing.suggested_social_share = (
-                    housing.building_id.social_share * housing.nb_rooms
+                    housing.building_id.social_share * housing.surface
                 )
             elif housing.building_id.regime == 'nb_rooms':
                 housing.suggested_social_share = (
-                    housing.building_id.social_share * housing.surface
+                    housing.building_id.social_share * housing.nb_rooms
                 )
             else:
                 raise ValidationError('Unknown building regime')
