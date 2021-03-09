9e081430727c93f0b3d3967a2899869590117693
robin.keunen <robin@keunen.net>
2021-03-09 15:25:15 +0100

    [FIX] hcb: demo data and tests

--- housing_cooperative_base/tests/test_housing_cooperative.py
+++ housing_cooperative_base/tests/test_housing_cooperative.py
@@ -10,6 +10,11 @@ from odoo.exceptions import ValidationError
 class HousingCooperativeCase(common.TransactionCase):
     def setUp(self):
         super(HousingCooperativeCase, self).setUp()
+        self.lease1 = self.env.ref("housing_cooperative_base.demo_lease_1")
+        self.lease2 = self.env.ref("housing_cooperative_base.demo_lease_2")
+        self.default_rent_product = self.env.ref("housing_cooperative_base.product_product_rent")
+        self.default_charges_product = self.env.ref("housing_cooperative_base.product_product_charges")
+
         self.housing1 = self.env.ref("housing_cooperative_base.demo_housing_1")
         self.housing2 = self.env.ref("housing_cooperative_base.demo_housing_2")
         self.cluster1 = self.env.ref("housing_cooperative_base.demo_cluster_1")
@@ -18,26 +23,31 @@ class HousingCooperativeCase(common.TransactionCase):
         self.room6 = self.env.ref("housing_cooperative_base.demo_room_6")
         self.cellar2 = self.env.ref("housing_cooperative_base.demo_cellar_2")
         self.parking2 = self.env.ref("housing_cooperative_base.demo_parking_2")
-        self.lease2 = self.env.ref("housing_cooperative_base.demo_lease_2")
 
-    def test_suggested_rent(self):
-        rent = (
-            self.housing2.rent
-            + self.room2.rent
-            + self.room6.rent
-            + self.cellar2.rent
-            + self.parking2.rent
-        )
-        self.assertEqual(self.lease2.suggested_rent, rent)
-
-    def test_create_contract(self):
-        self.assertFalse(self.lease2.contract_id)
-
+    def test_contract_product_set_to_default(self):
         self.lease2.create_contract()
 
+        contract_lines = self.lease2.contract_id.contract_line_ids
+        # any line will do for test purpose
+        # we might need to be more precise in the future
+        rent_line = contract_lines.filtered(lambda l: "rent" in l.name)[0]
+        charges_line = contract_lines.filtered(lambda l: "charges" in l.name)[0]
+
+        self.assertEqual(rent_line.product_id, self.default_rent_product)
+        self.assertEqual(charges_line.product_id, self.default_charges_product)
+
+    def test_contract_product_set_to_premise_product(self):
+        self.lease1.create_contract()
+
+        contract_lines = self.lease1.contract_id.contract_line_ids
+        rent_line = contract_lines.filtered(lambda l: "rent" in l.name)[0]
+        charges_line = contract_lines.filtered(lambda l: "charges" in l.name)[0]
+
+        self.assertEqual(rent_line.product_id, self.housing1.rent_product_id)
+        self.assertEqual(charges_line.product_id, self.housing1.charges_product_id)
+
+    def test_create_second_contract_fails(self):
+        self.assertFalse(self.lease2.contract_id)
+        self.lease2.create_contract()
         with self.assertRaises(ValidationError):
             self.lease2.create_contract()
-
-    # def test_adding_room_of_cluster_to_housing_error(self):
-    #     with self.assertRaises(ValidationError):
-    #         self.room1.housing_id = self.housing1
